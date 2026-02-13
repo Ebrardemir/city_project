acd9a5f9c72c3aab561ff3933ed0df6012e10e8d
Ebrardemir <ebrardemir6106@gmail.com>
2026-02-13 20:38:13 +0300

    proje yapısı oluşturuldu

--- city_project/lib/core/Theme/theme_provider.dart
+++ city_project/lib/core/Theme/theme_provider.dart
@@ -2,26 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
-  // ignore: constant_identifier_names
   static const THEME_STATUS = "THEME_STATUS";
-  bool _darkTheme = false;
+  bool _isDarkMode = false;
 
-  bool get isDarkTheme => _darkTheme;
+  bool get isDarkTheme => _isDarkMode;
+
+  // HATAYI ÇÖZEN KISIM: main.dart bu 'currentTheme'i bekliyor
+  ThemeData get currentTheme => _isDarkMode ? _darkTheme : _lightTheme;
 
   ThemeProvider() {
     getTheme();
   }
 
+  // Aydınlık ve Karanlık Tema Tanımları
+  final _lightTheme = ThemeData(brightness: Brightness.light, primarySwatch: Colors.blue);
+  final _darkTheme = ThemeData(brightness: Brightness.dark, primarySwatch: Colors.blue);
+
   Future<void> toggleTheme() async {
-    _darkTheme = !_darkTheme;
+    _isDarkMode = !_isDarkMode;
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(THEME_STATUS, _darkTheme);
+    await prefs.setBool(THEME_STATUS, _isDarkMode);
     notifyListeners();
   }
 
   Future<void> getTheme() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    _darkTheme = prefs.getBool(THEME_STATUS) ?? false;
+    _isDarkMode = prefs.getBool(THEME_STATUS) ?? false;
     notifyListeners();
   }
 }
