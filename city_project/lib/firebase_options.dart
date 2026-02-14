a6035937dfcdf7e25eafa255d42aa48dd1b1f1e8
hbhdr <210229046@kocaeli.edu.tr>
2026-02-14 15:25:48 +0300

    firebase test

--- city_project/lib/firebase_options.dart
+++ city_project/lib/firebase_options.dart
@@ -13,7 +13,7 @@ import 'package:flutter/foundation.dart'
 /// await Firebase.initializeApp(
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
-/// ```
+/// ```test
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
